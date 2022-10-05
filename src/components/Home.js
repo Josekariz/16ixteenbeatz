01f29e29a7717c6059b5edb9b1377c853314077f
Josekariz <joseph.macharia@student.moringaschool.com>
2022-10-05 23:20:27 +0300

    create a home component

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome <br/> To <br/> 16ixTeenBeatz </h1>
+    </div>
+  );
+}
+
+export default Home;

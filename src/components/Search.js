a46edde850b0e6b4eb7f8d378ec219f55f2eaec7
Josekariz <joseph.macharia@student.moringaschool.com>
2022-10-05 23:29:03 +0300

    add breaking point to search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,16 +2,15 @@ import React from "react";
 function Search({ search, setSearch }) {
   return (
     <div className="search">
-      Search
+      <br/>
       <input
         type="text"
         placeholder="search your music"
         onChange={(e) => {
           setSearch(e.target.value);
-          console.log(search);
         }}
       />
-      <span className="material-symbols-outlined">search</span>
+      <br/>
     </div>
   );
 }

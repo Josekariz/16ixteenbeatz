112b49e63a06c7238ace21533290c7ff8680234d
Josekariz <joseph.macharia@student.moringaschool.com>
2022-10-05 23:27:19 +0300

    remove h2 element in musicPage component

--- src/components/MusicPage.js
+++ src/components/MusicPage.js
@@ -5,7 +5,6 @@ function MusicPage() {
   const [search, setSearch] = useState("");
   return (
     <div className="musicpage">
-      <h2>music</h2>
       <Search search={search} setSearch={setSearch} />
       <div className="spit">
         <MusicList search={search} />

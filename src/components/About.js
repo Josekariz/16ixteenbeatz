a4d0d0a30cc15095ead17f0377f81490a3c1b26a
Josekariz <joseph.macharia@student.moringaschool.com>
2022-10-05 23:32:02 +0300

    create an about component having details on the app

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+function About() {
+  return (
+    <div className="about">
+      <h1>Less reading enjoy our music through the player.</h1>
+      <h3>
+        The app is dedicated to giving a unique experience when it comes to
+        music. <br /> The music library is universal. <br /> Play a song and
+        enjoy listening to music without ads. <em>@16ixTeenBeatz</em>
+      </h3>
+    </div>
+  );
+}
+
+export default About;
